8ce05f51c46e4c198606133701a483c7614bebc3
Shantanu Bhattacharyya <bioshantanu@gmail.com>
2025-06-26 04:43:53 +0530

    Create assignment1_tutedude.py

---
+++ assignment1_tutedude.py
@@ -0,0 +1,16 @@
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+A = a + b
+S = a - b
+M = a * b
+D = a / b if b != 0 else 'Undefined (division by zero)'
+print('Addition:', A)
+print('Subtraction:', S)
+print('Multiplication:', M)
+print('Division:', D)
+'''
+a = input('Enter your First Name: ')
+b = input('Enter your Last Name: ')
+c = a + " " + b
+print('Hello,', c, 'Welcome to Python Program')
+'''
